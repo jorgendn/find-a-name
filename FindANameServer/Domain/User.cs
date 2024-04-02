0b2aa4b3decc420efecd5429f55a90a86ff1dafc
Jørgen Drejer Norheim <jorgendt@gmail.com>
2024-04-02 14:47:06 +0200

    Start server development

---
+++ FindANameServer/Domain/User.cs
@@ -0,0 +1,7 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace FindANameServer.Domain;
+
+public class User : IdentityUser
+{
+}
---
+++ FindANameServer/Infrastructure/NameDbContext.cs
@@ -0,0 +1,9 @@
+﻿using FindANameServer.Domain;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace FindANameServer.Infrastructure;
+
+public class NameDbContext(DbContextOptions<NameDbContext> options) : IdentityDbContext<User>(options)
+{
+}
