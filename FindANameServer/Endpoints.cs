8403622693438a3d493aa0ffdaa6a24bca1c376b
Jørgen Drejer Norheim <jorgendt@gmail.com>
2024-04-02 15:43:29 +0200

    Start adding endpoints

---
+++ FindANameServer/Endpoints.cs
@@ -0,0 +1,17 @@
+﻿namespace FindANameServer;
+
+public static class Endpoints
+{
+    public static void AddNameEndpoints(this WebApplication app)
+    {
+        app.MapGet("/names", () =>
+        {
+            List<string> names = ["Jakob", "Jens", "Kolera"];
+
+            return names;
+        })
+        .WithName("GetNames")
+        .WithOpenApi()
+        .RequireAuthorization();
+    }
+}
--- FindANameServer/Program.cs
+++ FindANameServer/Program.cs
@@ -1,6 +1,8 @@
+using FindANameServer;
 using FindANameServer.Domain;
 using FindANameServer.Infrastructure;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -30,30 +32,18 @@ app.MapIdentityApi<User>();
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
+app.AddNameEndpoints();
 
-app.MapGet("/weatherforecast", () =>
+app.MapPost("/logout", async (SignInManager<User> signInManager, [FromBody] object empty) =>
 {
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
+    if (empty != null)
+    {
+        await signInManager.SignOutAsync();
+        return Results.Ok();
+    }
+    return Results.Unauthorized();
 })
-.WithName("GetWeatherForecast")
 .WithOpenApi()
 .RequireAuthorization();
 
 app.Run();
-
-internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
