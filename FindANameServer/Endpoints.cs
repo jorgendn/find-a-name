1c0e61a5f25523bf4e6b7528eda99f958dbd8e13
Jørgen Drejer Norheim <jorgendt@gmail.com>
2024-04-02 15:48:36 +0200

    Add post endpoint

--- FindANameServer/Endpoints.cs
+++ FindANameServer/Endpoints.cs
@@ -1,17 +1,29 @@
-﻿namespace FindANameServer;
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FindANameServer;
 
 public static class Endpoints
 {
+    private static List<string> names = ["Jakob", "Jens", "Kolera"];
+
     public static void AddNameEndpoints(this WebApplication app)
     {
         app.MapGet("/names", () =>
         {
-            List<string> names = ["Jakob", "Jens", "Kolera"];
-
             return names;
         })
         .WithName("GetNames")
-        .WithOpenApi()
-        .RequireAuthorization();
-    }
+        .WithOpenApi();
+
+        app.MapPost("/names", ([FromBody] string[] newNames) => {
+            foreach (var name in newNames)
+            {
+                names.Add(name);
+            }
+
+            return Results.Ok();
+        })
+        .WithName("PostNames")
+        .WithOpenApi();
+    }   
 }
